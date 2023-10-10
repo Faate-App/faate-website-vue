6358a4ce846f978973c6dab74250eef6ccd0ce10
Leonard Roussard <lroussard@gmail.com>
2023-10-10 19:07:38 +0200

    feat: base structure

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Home page content goes here.</p>
+    <p>Home page content goes here.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
