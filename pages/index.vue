2a37493d876433698beeb29b77520e5a20001f78
Leonard Roussard <lroussard@gmail.com>
2023-10-11 19:36:35 +0200

    feat: modal component

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,18 @@
     <div>
       <h1 class="mb-6 text-4xl">Flowbite + Nuxt.js + Tailwind CSS Starter</h1>
       <div class="w-auto mb-4 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <NuxtLink to="/modal" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">Modal</NuxtLink>
       </div>
       <p class="mb-8">Learn more about <a href="https://flowbite.com/docs/getting-started/nuxt-js/" class="text-blue-600 hover:underline">Flowbite + Nuxt.js here</a>.</p>
+      <Modal :modalInfo="{
+        title: 'Modal title',
+        body: 'Modal body text goes here.',
+        buttonTitle: 'Open modal',
+        acceptLabel: 'Tamer',
+        declineLabel: 'Decline'
+      }" />
     </div>
   </div>
 </template>
+
+<script setup>
+</script>
