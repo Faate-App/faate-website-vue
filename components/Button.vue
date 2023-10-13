efa034c454eae245387e29a727b57e89531f7275
Leonard Roussard <lroussard@gmail.com>
2023-10-13 17:56:45 +0200

    feat(home): learn more section

--- components/Button.vue
+++ components/Button.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-  const { background, text } = defineProps(['background', 'text'])
+  const { background, text, reference } = defineProps(['background', 'text', 'reference'])
 </script>
 
 <template>
-  <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-accent hover:bg-tertiary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+  <a :href="reference" class="inline-flex justify-center items-center py-2 px-5 text-base font-medium text-center text-white rounded-lg bg-accent hover:bg-tertiary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
     <slot></slot>
   </a>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,11 @@ module.exports = {
       headline: '#2C2C2C', // Dark color for text to ensure readability
       paragraph: '#4B4B4B', // Slightly lighter text color for contrast
       secondary: '#FFC75F', // Warm secondary color to make CTAs stand out
-      tertiary: '#FF61A6' // A vibrant pink to add a playful, romantic vibe
+      tertiary: '#FF61A6', // A vibrant pink to add a playful, romantic vibe
+      darkBackground: '#1e1e2b',  // Darker background
+      darkHeadline: '#E0E0E0',  // Light color for text on dark background
+      darkParagraph: '#B0B0B0',  // Lighter text color for contrast
+      darkBlock: '#2E2E3D',
     }
   },
   plugins: [
