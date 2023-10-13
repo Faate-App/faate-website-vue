e2b5f92080ad7065b7d3e2976d451add19d4ed54
Poliaboy <szpakiewicz.alex@gmail.com>
2023-10-13 15:31:28 +0200

    feat : WHats new working fix : modal with tally working

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-        <WhatsNew message="Rejoignez notre waitlist" reference="#" />
+        <WhatsNew message="Rejoignez notre waitlist" reference="#hero" />
       <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
       <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
       <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
@@ -14,11 +14,19 @@
           Watch video
         </a>
       </div>
+
+      <ClientOnly id="hero">
+        <ModalWTally :modalFormInfo="{
+        title: 'Datee waitlist',
+        buttonTitle: 'Rejoignez notre waitlist',
+        }" />
+      </ClientOnly>
     </div>
   </section>
 </template>
 
 <script setup>
+import ModalWTally from "~/components/ModalWTally.vue";
 </script>
 
 
