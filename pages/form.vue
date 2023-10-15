0879a04457a858bf3d54f26111c6685a9f8b6d9f
Poliaboy <szpakiewicz.alex@gmail.com>
2023-10-15 20:37:20 +0200

    feat: center tally team member alex

--- pages/form.vue
+++ pages/form.vue
@@ -6,11 +6,19 @@
       <p class="mb-8 text-lg font-normal text-paragraph lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
         Chez Datee nous travaillons à vous offrir une expérience plus fluide que les applications de rencontre
         traditionnelles. Notre solution vous promet un match personnalisé par nos soins par tranche de 2 semaines.</p>
-      <client-only placeholder="loading...">
-        <iframe data-tally-src="https://tally.so/embed/w2AxlL?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-                loading="lazy" width="100%" height="302" frameborder="0" marginheight="0" marginwidth="0"
+      <div>
+        <client-only placeholder="loading...">
+          <iframe data-tally-src="https://tally.so/embed/w2AxlL?alignCenter=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+                loading="lazy" width="100%" height="302"
                 title="Website waitlist"></iframe>
-      </client-only>
+        </client-only>
+      </div>
+      <NuxtLink to="/" class="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-accent hover:bg-blue-800 focus:ring-4 focus focus:ring-blue-900">
+        Retour à l'acceuil
+        <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+      </NuxtLink>
 
     </div>
   </section>
