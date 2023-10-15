67a59488305b83ff77a3352bed5933ebbac272c6
Poliaboy <szpakiewicz.alex@gmail.com>
2023-10-15 20:39:55 +0200

    fix: readable script for tally form

--- pages/form.vue
+++ pages/form.vue
@@ -26,7 +26,17 @@
 </template>
 <script setup>
 if (typeof document !== 'undefined') {
-  var d=document, w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}
+  var d=document;
+  w="https://tally.so/widgets/embed.js";
+  v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};
+  if("undefined"!=typeof Tally)v();
+  else if(d.querySelector('script[src="'+w+'"]')==null)
+  {
+    var s=d.createElement("script");
+    s.src=w;
+    s.onload=v;
+    s.onerror=v;
+    d.body.appendChild(s);}
 
 
   // Manipulating the DOM here
