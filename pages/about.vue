80560501fc8de57d79d87953db4f017d325efce5
Poliaboy <szpakiewicz.alex@gmail.com>
2023-10-17 23:33:01 +0200

    feat: header+ footer privacy and about template layout and index implementation of changes

---
+++ pages/about.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="bg-background h-auto" id="about-us">
+    <div class="py-8 px-4 mx-auto lg:max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-headline md:text-5xl lg:text-6xl dark:text-white">About Us</h1>
+      <p class="mb-8 text-lg font-normal text-paragraph lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Get to Know [Your Company Name]</p>
+      <div class="text-left text-lg font-normal text-paragraph lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        <h2 class="mb-4 text-2xl font-bold">Our Story</h2>
+        <p>Welcome to [Your Company Name] – your trusted partner in [Your Industry]. We take pride in providing top-quality [Your Products/Services] to our valued customers. Our journey began with a passion for [Your Mission or Purpose], and today, we stand as a leading name in the industry.</p>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Our Commitment</h2>
+        <p>At [Your Company Name], we are committed to [Your Core Values or Principles]. Our dedicated team works tirelessly to ensure that you receive the best possible [Your Products/Services] that meet and exceed your expectations. We believe in [Your Beliefs or Philosophy].</p>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Why Choose Us</h2>
+        <p>When you choose [Your Company Name], you are choosing excellence. Here are some reasons why our customers love us:</p>
+        <ul class="list-disc list-inside">
+          <li>[Highlight a key benefit or feature]</li>
+          <li>[Highlight another key benefit or feature]</li>
+          <li>[Highlight another key benefit or feature]</li>
+        </ul>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Our Team</h2>
+        <p>Meet the talented individuals who drive [Your Company Name] forward. Our team comprises experts in [Your Industry] who are passionate about what they do. Together, we strive to innovate and deliver excellence.</p>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Contact Us</h2>
+        <p>If you have any questions or would like to learn more about [Your Company Name], please don't hesitate to reach out. We're here to assist you!</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+useHead({
+  script: [
+    { async: true,
+      src: 'https://tally.so/widgets/embed.js',
+    }
+  ],
+});
+</script>
+```
---
+++ pages/privacy.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="bg-background h-auto" id="privacy-policy">
+    <div class="py-8 px-4 mx-auto lg:max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-headline md:text-5xl lg:text-6xl dark:text-white">Privacy Policy</h1>
+      <p class="mb-8 text-lg font-normal text-paragraph lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Effective Date: [Date]</p>
+      <div class="text-left text-lg font-normal text-paragraph lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        <h2 class="mb-4 text-2xl font-bold">Introduction</h2>
+        <p>Welcome to [Your Website/Service] ("we," "our," or "us"). At [Your Website/Service], we value and prioritize your privacy. This Privacy Policy outlines how we collect, use, disclose, and protect your personal information when you use our website or services.</p>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Information We Collect</h2>
+        <p>We may collect various types of information from you, including:</p>
+        <ul class="list-disc list-inside">
+          <li>Personal Information: [Description of personal information collected]</li>
+          <li>Usage Information: [Description of usage information collected]</li>
+          <li>Cookies and Tracking Technologies: [Description of cookies and tracking technologies used]</li>
+        </ul>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">How We Use Your Information</h2>
+        <p>We use the collected information for various purposes, including:</p>
+        <ul class="list-disc list-inside">
+          <li>[Description of how personal information is used]</li>
+          <li>[Description of how usage information is used]</li>
+          <li>[Description of how cookies and tracking technologies are used]</li>
+        </ul>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Disclosure of Your Information</h2>
+        <p>We may disclose your information to third parties in certain circumstances, including:</p>
+        <ul class="list-disc list-inside">
+          <li>[Description of circumstances under which information is disclosed]</li>
+          <li>[List of third parties with whom information may be shared]</li>
+        </ul>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Your Choices</h2>
+        <p>You have choices regarding the information we collect and how it is used, including:</p>
+        <ul class="list-disc list-inside">
+          <li>[Description of user choices and controls]</li>
+        </ul>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Security</h2>
+        <p>We take reasonable measures to protect your information, but no method of transmission over the internet is 100% secure. We cannot guarantee the security of your information.</p>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Changes to This Privacy Policy</h2>
+        <p>We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated effective date. Please review this Privacy Policy periodically for updates.</p>
+
+        <h2 class="mt-8 mb-4 text-2xl font-bold">Contact Us</h2>
+        <p>If you have any questions or concerns about this Privacy Policy, please contact us at [Contact Information].</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+useHead({
+  script: [
+    { async: true,
+      src: 'https://tally.so/widgets/embed.js',
+    }
+  ],
+});
+</script>
